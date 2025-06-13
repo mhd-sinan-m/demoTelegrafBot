9741ca4040fa8d0ec5f59e856201ad0c05c1305b
Muhammed Sinan <muhammedsinankzp@gmail.com>
2025-06-13 19:19:20 +0530

    Update vercel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Telegraf } from 'telegraf'
+import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 import { message } from 'telegraf/filters';
 import * as controller from "./controller.js";
@@ -11,21 +11,20 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(controller.startMsg);
 
 // Other messages
-bot.hears(/'hi'/i, (ctx) => ctx.reply('Hey there'))
+bot.hears(/hi/i, (ctx) => ctx.reply('Hey there')); // Removed quotes around 'hi'
 bot.on(message('text'), controller.replyToText);
 bot.on(message('photo'), controller.replyToPhoto);
 
-// Launch
-// bot.launch();
-
-console.log('\x1b[31m%s\x1b[0m', 'BOT RUNNING...');
+// bot.launch() 
+console.log('\x1b[32m%s\x1b[0m', 'BOT CONFIGURED...'); // Changed color to green for "configured" status
 
 module.exports = async (req, res) => {
   try {
     await bot.handleUpdate(req.body);
     res.status(200).send('OK');
+    console.log('UPDATE HANDLED SUCCESSFULLY...');
   } catch (error) {
-    console.error(error);
+    console.error('ERROR OCCURRED:', error); 
     res.status(500).send('Error');
   }
 };
