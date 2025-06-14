e1eaef532ba7d4b6528e2f94ef879d18feb4b079
Muhammed Sinan <muhammedsinankzp@gmail.com>
2025-06-14 11:27:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,4 @@ bot.start(controller.startMsg);
 bot.hears(/hi/i, (ctx) => ctx.reply('Hey there')); // Removed quotes around 'hi'
 bot.on(message('text'), controller.replyToText);
 bot.on(message('photo'), controller.replyToPhoto);
-
-
-console.log('\x1b[32m%s\x1b[0m', 'BOT CONFIGURED...'); // Changed color to green for "configured" status
-
-bot.launch() 
-//module.exports = async (req, res) => {
-//try {
-//  await bot.handleUpdate(req.body);
-//  res.status(200).send('OK');
-  //  console.log('UPDATE HANDLED SUCCESSFULLY...');
-  //} catch (error) {
-   // console.error('ERROR OCCURRED:', error); 
-    //res.status(500).send('Error');
- // }};
+bot.launch()
