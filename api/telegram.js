23da0e8363f23b6ba5f7f93c0d9ad8a97a4151ca
Muhammed Sinan <muhammedsinankzp@gmail.com>
2025-06-15 19:47:49 +0530

    Update and rename index.js to api/telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,27 +1,25 @@
 import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
-import { message } from 'telegraf/filters';
-import * as controller from "./controller.js";
+import * as controller from '../controller.js';
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Start
+// Register handlers
 bot.start(controller.startMsg);
+bot.hears(/hi/i, (ctx) => ctx.reply('Hey there'));
+bot.on('text', controller.replyToText);
+bot.on('photo', controller.replyToPhoto);
 
-// Other messages
-bot.hears(/hi/i, (ctx) => ctx.reply('Hey there')); // Removed quotes around 'hi'
-bot.on(message('text'), controller.replyToText);
-bot.on(message('photo'), controller.replyToPhoto);
-
-// vercel handler
+// Vercel Serverless Function Handler
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       await bot.handleUpdate(req.body);
       res.status(200).send('OK');
     } catch (err) {
+      console.error('Error handling update:', err);
       res.status(500).send('Error handling update');
     }
   } else {
